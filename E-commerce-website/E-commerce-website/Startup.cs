e7dff0bd0c4b915eaca200436c2dbe93d6396f9d
Nadamostafa2 <48297055+Nadamostafa2@users.noreply.github.com>
2022-04-06 20:06:16 +0200

    SendConfirmation&resetpassEmails

--- E-commerce-website/E-commerce-website/Startup.cs
+++ E-commerce-website/E-commerce-website/Startup.cs
@@ -33,15 +33,14 @@ namespace E_commerce_website
 
             services.AddHttpContextAccessor();
             services.AddDbContext<OnlineshoppingContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddTransient<IEmailSender, EmailSender>();
-
-
+           
+            
             services.Configure<StripeSettings>(Configuration.GetSection("Stripe"));
-
+            services.AddTransient<IEmailSender, EmailSender>();
             #region Repository DI
-                
-                // Products
-                services.AddScoped<IProductRepository, ProductRepository>();
+
+            // Products
+            services.AddScoped<IProductRepository, ProductRepository>();
                 services.AddScoped<IProductOptionRepository, ProductOptionRepository>();
                 services.AddScoped<IProductCategoryRepository, ProductCategoryRepository>();
 
@@ -84,7 +83,19 @@ namespace E_commerce_website
             services.AddScoped<IUserService, UserService>();
 
             #endregion
-
+            #region External Logins
+            services.AddAuthentication().AddGoogle(op =>
+            {
+                IConfigurationSection googleAuthSection = Configuration.GetSection("Authentication:Google");
+                op.ClientId = googleAuthSection["ClientId"];
+                op.ClientSecret = googleAuthSection["ClientSecret"];
+            }).AddFacebook(op =>
+            {
+                IConfigurationSection facebookAuthSection = Configuration.GetSection("Authentication:FaceBook");
+                op.ClientId = facebookAuthSection["ClientId"];
+                op.ClientSecret = facebookAuthSection["ClientSecret"];
+            });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
